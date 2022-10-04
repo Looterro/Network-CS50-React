904255c4e5f1332b7b18e7bb057b4cd7e1f7420f
Jakub Łata <lootjakub@gmail.com>
2022-10-04 18:00:09 +0200

    configured react index page

--- network/static/network/index.jsx
+++ network/static/network/index.jsx
@@ -1,1 +1,15 @@
 //TODO
+
+function App() {
+    const name = 'Bryan';
+    const last = 'token';
+    return (
+        // load an app component
+        <div>
+            hello
+        </div>
+    );
+}
+
+//Render app component and show where to render it
+ReactDOM.render(<App />, document.querySelector("#app"));
